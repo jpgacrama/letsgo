c49879b0546d8247cb681d79fdef1fdfb20c0200
Jonas Gacrama <jonasgacrama@gmail.com>
2022-12-20 17:47:43 +0800

    webApp: Created other routes

--- webApp/src/main.go
+++ webApp/src/main.go
@@ -9,10 +9,29 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func main() {
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func createRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	if mux != nil {
+		mux.HandleFunc("/", home)
+		mux.HandleFunc("/snippet", showSnippet)
+		mux.HandleFunc("/snippet/create", createSnippet)
+	} else {
+		log.Fatal("Cannot create Server MUX")
+	}
+	return mux
+}
+
+func main() {
 	log.Println("Starting server on :4000")
+	mux := createRoutes()
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
