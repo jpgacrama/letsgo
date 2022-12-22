1527ed4bb4129dfe87eea93052a4f45ffe18f92b
Jonas Gacrama <jonasgacrama@gmail.com>
2022-12-22 17:42:14 +0800

    snippetbox: created test code for showing snippet
    It doesn't work now, so I just commented it out

--- snippetbox/test/main_test.go
+++ snippetbox/test/main_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
+func TestHomePage(t *testing.T) {
 	t.Run("checking home page OK Case", func(t *testing.T) {
 		handlers.TemplateFile = "../ui/html/home.page.tmpl"
 		server, err := handlers.CreateServer()
@@ -32,13 +32,34 @@ func TestHandler(t *testing.T) {
 		server.GetHandler().ServeHTTP(response, request)
 		assertStatus(t, response, http.StatusNotFound)
 	})
-
 }
 
-func newRequest(str string) *http.Request {
-	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/%s", str), nil)
-	return req
+// func TestShowSnippet(t *testing.T) {
+// 	t.Run("checking show snippet OK Case", func(t *testing.T) {
+// 		server, err := handlers.CreateServer()
+// 		if err != nil {
+// 			log.Fatalf("problem creating server %v", err)
+// 		}
+// 		request := newRequest("/snippet")
+// 		response := httptest.NewRecorder()
+// 		server.GetHandler().ServeHTTP(response, request)
+// 		assertStatus(t, response, http.StatusOK)
+// 	})
+// 	t.Run("checking show snippet NOK Case", func(t *testing.T) {
+// 		server, err := handlers.CreateServer()
+// 		if err != nil {
+// 			log.Fatalf("problem creating server %v", err)
+// 		}
+// 		request := newRequest("123")
+// 		response := httptest.NewRecorder()
+// 		server.GetHandler().ServeHTTP(response, request)
+// 		assertStatus(t, response, http.StatusNotFound)
+// 	})
+// }
 
+func newRequest(str string) *http.Request {
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%s", str), nil)
+	return req
 }
 
 func assertStatus(t testing.TB, response *httptest.ResponseRecorder, want int) {
