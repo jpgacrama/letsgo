bedab0da42bba14ed631b528a7495a051ff37ca8
Jonas Gacrama <jonasgacrama@gmail.com>
2022-12-23 10:54:16 +0800

    snippetbox: I now allow to define different ports
    I need to create tests for this.

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"snippetbox/cmd/server"
 )
 
 func main() {
-	log.Println("Starting server on :4000")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	server, err := server.CreateServer()
 	if err == nil {
-		log.Fatal(http.ListenAndServe(":4000", server.GetMux()))
+		log.Printf("Starting server on %s", *addr)
+		log.Fatal(http.ListenAndServe(*addr, server.GetMux()))
 	}
 }
