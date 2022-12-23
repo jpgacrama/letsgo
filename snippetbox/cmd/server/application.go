a7312186335a15a75f49aa2ac6c7d7c886e2d0ff
Jonas Gacrama <jonasgacrama@gmail.com>
2022-12-23 17:40:59 +0800

    snippetbox: created wrappers for sending error messages

---
+++ snippetbox/cmd/server/application.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"runtime/debug"
+	"strconv"
+)
+
+type Application struct {
+	Addr     *string
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+}
+
+func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	ts, err := template.ParseFiles(templateFiles...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+
+func (app *Application) ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func (app *Application) CreateSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func (app *Application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.ErrorLog.Println(trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *Application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *Application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
--- snippetbox/cmd/server/server.go
+++ snippetbox/cmd/server/server.go
@@ -2,10 +2,7 @@ package server
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"strconv"
 )
 
 var templateFiles = []string{
@@ -15,12 +12,6 @@ var templateFiles = []string{
 }
 
 var StaticFolder = "./ui/static"
-
-type Application struct {
-	Addr     *string
-	ErrorLog *log.Logger
-	InfoLog  *log.Logger
-}
 
 func CreateServer(app *Application, tmplFiles ...string) (*http.Server, error) {
 	if len(tmplFiles) > 0 {
@@ -45,9 +36,9 @@ func CreateServer(app *Application, tmplFiles ...string) (*http.Server, error) {
 func createRoutes(fileServer http.Handler, app *Application) (*http.ServeMux, error) {
 	mux := http.NewServeMux()
 	if mux != nil {
-		mux.HandleFunc("/", app.home)
-		mux.HandleFunc("/snippet", app.showSnippet)
-		mux.HandleFunc("/snippet/create", app.createSnippet)
+		mux.HandleFunc("/", app.Home)
+		mux.HandleFunc("/snippet", app.ShowSnippet)
+		mux.HandleFunc("/snippet/create", app.CreateSnippet)
 		mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	} else {
 		return nil, fmt.Errorf("cannot create Handler")
@@ -57,41 +48,4 @@ func createRoutes(fileServer http.Handler, app *Application) (*http.ServeMux, er
 
 func createFileServer() http.Handler {
 	return http.FileServer(http.Dir(StaticFolder))
-}
-
-func (app *Application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	ts, err := template.ParseFiles(templateFiles...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Template file not found", http.StatusNotFound)
-		return
-	}
-	err = ts.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error: Cannot execute Template", http.StatusInternalServerError)
-	}
-}
-
-func (app *Application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-func (app *Application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
 }
