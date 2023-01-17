f3cb6cadc0af0827da518315eec42ad5bb7dc5a2
Jonas Gacrama <jonasgacrama@gmail.com>
2023-01-17 11:44:44 +0800

    snippetbox: Created Unit tests for SQL from scratch

--- snippetbox/test/database_test.go
+++ snippetbox/test/database_test.go
@@ -2,52 +2,20 @@ package snippetbox_test
 
 import (
 	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
 	"log"
-	"net/http"
-	"net/http/httptest"
-	"os"
-	"snippetbox/cmd/server"
-	"snippetbox/pkg/models/mysql"
 	"testing"
 )
 
-func openDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
+func NewMock() (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
 	if err != nil {
-		return nil, err
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
+	return db, mock
 }
 
-func TestCreateSnippet(t *testing.T) {
-	addr := ":4000"
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	db, _ := NewMock()
-	app := &server.Application{
-		Addr:     &addr,
-		InfoLog:  infoLog,
-		ErrorLog: errorLog,
-		Snippets: &mysql.SnippetModel{DB: db}, // mock DB
-		SqlRecord: &server.Record{
-			Title:   "O snail",
-			Content: "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa",
-			Expires: "7",
-		},
-	}
+func TestInsert(t *testing.T) {
 
-	t.Run("creating snippet OK Case", func(t *testing.T) {
-		server, err := server.CreateServer(app, templateFiles...)
-		if err != nil {
-			log.Fatalf("problem creating server %v", err)
-		}
-		request := newRequest(http.MethodPost, "snippet/create")
-		response := httptest.NewRecorder()
-		server.Handler.ServeHTTP(response, request)
-		assertStatus(t, response, http.StatusOK)
-	})
 }
