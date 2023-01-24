2074a7f5f9e43db5e955d764904b5c1113a3b071
Jonas Gacrama <jonasgacrama@gmail.com>
2023-01-24 18:30:39 +0800

    snippetbox: Used loggers from helpers.go
    This is now used to print Info and Error Logs

--- snippetbox/pkg/models/mysql/snippets.go
+++ snippetbox/pkg/models/mysql/snippets.go
@@ -17,39 +17,41 @@ func (m *SnippetDatabase) Close() {
 }
 
 func (m *SnippetDatabase) Insert(title, content, expires string) (int, error) {
+	m.InfoLog.Println("--- Inside Insert() ---")
 	query := `INSERT INTO snippets (title, content, created, expires)
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 	stmt, err := m.DB.Prepare(query)
 	if err != nil {
-		println("\t--- Insert(): Error Preparing Statement ---")
+		m.ErrorLog.Println("\t--- Insert(): Error Preparing Statement ---")
 		return 0, err
 	}
 	defer stmt.Close()
 
 	result, err := stmt.Exec(title, content, expires)
 	if err != nil {
-		println("\t--- Insert(): Error Executing Context ---")
+		m.ErrorLog.Println("\t--- Insert(): Error Executing Context ---")
 		return 0, err
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		println("\t--- Insert(): Getting Last Insert ID ---")
+		m.ErrorLog.Println("\t--- Insert(): Error Getting Last Insert ID ---")
 		return 0, err
 	}
 	return int(id), nil
 }
 
 func (m *SnippetDatabase) Get(id int) (*models.Snippet, error) {
+	m.InfoLog.Println("--- Inside Get() ---")
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 	row := m.DB.QueryRow(stmt, id)
 	s := &models.Snippet{}
 	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err == sql.ErrNoRows {
-		println("\t--- Get(): No Record ---")
+		m.ErrorLog.Println("\t--- Get(): No Record ---")
 		return nil, models.ErrNoRecord
 	} else if err != nil {
-		println("\t--- Get(): Error Scanning ---")
+		m.ErrorLog.Println("\t--- Get(): Error Scanning ---")
 		return nil, err
 	}
 	return s, nil
