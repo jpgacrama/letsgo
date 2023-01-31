c914e170f7bf3ec37cb2d3b6e19ad17e0d5215b9
Jonas Gacrama <jonasgacrama@gmail.com>
2023-01-31 21:28:44 +0800

    dynamicData: Removed comment for Created
    It is being used when querying

--- snippetbox/pkg/models/models.go
+++ snippetbox/pkg/models/models.go
@@ -11,6 +11,6 @@ type Snippet struct {
 	ID      int
 	Title   string
 	Content string
-	Created time.Time // This is not used by SQL Statements. It just uses UTC_TIMESTAMP()
+	Created time.Time
 	Expires time.Time
 }
