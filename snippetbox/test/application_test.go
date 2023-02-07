f47a49a96cc4bfde64836370675505cb7d863d63
Jonas Gacrama <jonasgacrama@gmail.com>
2023-02-07 20:34:49 +0800

    dataValidation: Created NOK case where Title in blank

--- snippetbox/test/application_test.go
+++ snippetbox/test/application_test.go
@@ -367,6 +367,30 @@ func TestCreateSnippet(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 		assertStatus(t, response, http.StatusBadRequest)
 	})
+	t.Run("checking create snippet NOK Case - Title is blank", func(t *testing.T) {
+		server, err := server.CreateServer(app)
+		if err != nil {
+			log.Fatalf("problem creating server %v", err)
+		}
+		request := newRequest(http.MethodPost, "snippet/create")
+		blankTitle := ""
+		request.PostForm = map[string][]string{
+			"title":   {blankTitle},
+			"content": {"Content"},
+			"expires": {"1"},
+		}
+		response := httptest.NewRecorder()
+
+		// Adding ExpectPrepare to DB Expectations
+		prep.ExpectExec().WithArgs(
+			blankTitle,
+			"Content",
+			"1",
+		).WillReturnResult(sqlmock.NewResult(0, 0))
+
+		server.Handler.ServeHTTP(response, request)
+		assertStatus(t, response, http.StatusBadRequest)
+	})
 }
 
 func newRequest(requestType, str string) *http.Request {
