7ab31cd9b0c258e915b4bd18cc7a06c52dc30721
Jonas Gacrama <jonasgacrama@gmail.com>
2023-02-15 16:22:46 +0800

    authentication: Removed extraneous comments in application.go

--- cmd/server/application.go
+++ cmd/server/application.go
@@ -184,26 +184,22 @@ func (app *Application) signupUserForm(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Application) signupUser(w http.ResponseWriter, r *http.Request) {
-	// Parse the form data.
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	// Validate the form contents using the form helper we made earlier.
 	form := forms.New(r.PostForm)
 	form.Required("name", "email", "password")
 	form.MatchesPattern("email", forms.EmailRX)
 	form.MinLength("password", 10)
 
-	// If there are any errors, redisplay the signup form.
 	if !form.Valid() {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	// Otherwise send a placeholder response (for now!).
 	fmt.Fprintln(w, "Create a new user...")
 }
 
